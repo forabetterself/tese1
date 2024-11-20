eeae12402e7f392ba2502f28f64d4d83b1757bad
forabetterself <142489334+forabetterself@users.noreply.github.com>
2024-11-20 10:54:22 +0800

    testcwe835.java

--- testcwe835.java
+++ testcwe835.java
@@ -3,7 +3,7 @@ public class testcwe835 {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; i < 10; j++) {
                 // 执行操作
-                System.out.println("i = " + i + ", j = " + j);
+                System.out.println(i);
                
             }
         }
