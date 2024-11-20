389d6cfe087ba45e80ac403a16bf668f156c2f43
forabetterself <142489334+forabetterself@users.noreply.github.com>
2024-11-20 10:49:12 +0800

    testcwe835.java

--- testcwe835.java
+++ testcwe835.java
@@ -1,6 +1,11 @@
-for (int i=0; i<10; i++) {
-    for (int j=0; i<10; j++) {
-        // do stuff
-        if (shouldBreak()) break;
+public class testcwe835 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                // 执行操作
+                System.out.println("i = " + i + ", j = " + j);
+               
+            }
+        }
     }
-}
+
